d545b4d7e6d113b938a09418585f0b474cc5fc88
Limon <146566739+Amirhossainlimon@users.noreply.github.com>
2025-06-13 02:05:32 +0600

    Add files via upload

---
+++ inverse triangle pattern.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+for(int i = 0; i < n; i++) {
+    // spaces
+    for(int j = 0; j < i; j++) {
+        cout<< " ";
+    }
+
+    // numbers
+    for(int j = 0; j < n - i; j++) {
+        cout << (i+1)<<" ";// i+1 means i er maan 1 beree use hbe kintu i te store korbe naa
+    }
+
+    cout << endl;
+}
+return 0;
+}
