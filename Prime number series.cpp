ab23ac2d8fbb3086f07bc032bbe64c5264101270
Amir Hossain Limon <146566739+Amirhossainlimon@users.noreply.github.com>
2025-07-23 01:39:52 +0600

    Add files via upload

---
+++ Prime number series.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, i, j;
+    cin >> n;
+
+    for (j = 2; j <= n; j++) {
+        for (i = 2; i <= j; i++) {
+            if (j % i == 0) {
+                break;
+            }
+        }
+        if (j == i) {
+            cout << i << endl;
+        }
+    }
+
+    return 0;
+}
