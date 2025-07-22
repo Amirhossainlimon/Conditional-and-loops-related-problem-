edd0ac2c7b75b97163abd2e94a30964c32aa8a51
Amir Hossain Limon <146566739+Amirhossainlimon@users.noreply.github.com>
2025-07-23 01:57:35 +0600

    Add files via upload

---
+++ Sort Three Numbers.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+int x,y,z;
+ cin>>x>>y>>z;
+if (x <= y && y <= z) {
+        cout << x << " " << y << " " << z << endl;
+    }
+    else if (x <= z && z <= y) {
+        cout << x << " " << z << " " << y << endl;
+    }
+    else if (y <= x && x <= z) {
+        cout << y << " " << x << " " << z << endl;
+    }
+    else if (y <= z && z <= x) {
+        cout << y << " " << z << " " << x << endl;
+    }
+    else if (z <= x && x <= y) {
+        cout << z << " " << x << " " << y << endl;
+    }
+    else {
+        cout << z << " " << y << " " << x << endl;
+    }
+
+
+return 0;
+}
